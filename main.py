9dd6f353971e074a77e441a137f2cdb61278a0d2
John Fish <john@johnafish.ca>
2018-02-22 14:02:02 -0500

    Added school command; refactor opening of URLS

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ CHECKIN_URLS = ["https://www.youtube.com/dashboard?o=U",
                 "https://affiliate-program.amazon.com/home",
                 "https://admin.mailchimp.com"]
 
+MAIL_URLS = ["https://mail.google.com/mail/u/" + str(i) for i in range(2)]
+
+SCHOOL_URLS = ["https://canvas.harvard.edu",
+               "https://calendar.google.com/calendar/r"]
+
 def parse_cmd(cmd):
     """ Parses string commands and shortcuts to functions """
     commands = {"c" : checkin,
@@ -18,20 +23,28 @@ def parse_cmd(cmd):
                 "quit" : sys.exit,
                 "m" : mail,
                 "mail" : mail,
-                "email" : mail}
+                "email" : mail,
+                "s" : school,
+                "school" : school}
     commands[cmd.lower()]()
 
-def checkin():
-    """ Opens earnings checkin URLs """
-    for url in CHECKIN_URLS:
+def open_urls(urls):
+    """ Opens a set of urls with an appropriate delay """
+    for url in urls:
         webbrowser.open(url, new=2)
         time.sleep(1)
 
+def checkin():
+    """ Opens earnings checkin URLs """
+    open_urls(CHECKIN_URLS)
+
 def mail():
     """ Opens both mail accounts of mine """
-    for i in range(2):
-        webbrowser.open("https://mail.google.com/mail/u/" + str(i), new=2)
-        time.sleep(1)
+    open_urls(MAIL_URLS)
+
+def school():
+    """ Opens my school related tabs """
+    open_urls(SCHOOL_URLS)
 
 if __name__ == "__main__":
     while True:
