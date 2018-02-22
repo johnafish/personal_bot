7a3c84086f031a728d3b7688be5d31acaff3bd17
John Fish <john@johnafish.ca>
2018-02-22 14:06:48 -0500

    Factored constants into seperate file

--- main.py
+++ main.py
@@ -2,16 +2,7 @@
 import webbrowser
 import time
 import sys
-
-CHECKIN_URLS = ["https://www.youtube.com/dashboard?o=U",
-                "https://my.questrade.com/trading/account/balances",
-                "https://affiliate-program.amazon.com/home",
-                "https://admin.mailchimp.com"]
-
-MAIL_URLS = ["https://mail.google.com/mail/u/" + str(i) for i in range(2)]
-
-SCHOOL_URLS = ["https://canvas.harvard.edu",
-               "https://calendar.google.com/calendar/r"]
+from constants import CHECKIN_URLS, MAIL_URLS, SCHOOL_URLS
 
 def parse_cmd(cmd):
     """ Parses string commands and shortcuts to functions """
